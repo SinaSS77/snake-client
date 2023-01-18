1c46ba7fe24777822ac9f55b2e93e59f8502a5d4
SinaSS77 <s.molaverdikhani>
2023-01-18 13:48:37 -0800

    Some changes

--- play.js
+++ play.js
@@ -1,25 +1,26 @@
 const net = require("net");
+const connect = require("./client");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "165.227.47.243",
+//     port: 50541,
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
+
+//   conn.on("data", (data) => {
+//     console.log("Server says: ", data);
+//   });
   
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
+//   conn.on("connect", () => {
+//     conn.write("Hello from client!");
+//   });
   
-  conn.on("connect", () => {
-    conn.write("Hello from client!");
-  });
-  
-  return conn;
-};
+//   return conn;
+// };
 console.log("Connecting ...");
 connect();
 
