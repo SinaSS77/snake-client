760a2eebfe9c5b2ae66f0fda4362377bc39b6057
SinaSS77 <s.molaverdikhani>
2023-01-18 19:01:05 -0800

    codes for paly

--- play.js
+++ play.js
@@ -1,10 +1,6 @@
-// const net = require("net");
-
 const connect = require("./client");
 const setupInput = require('./input');
 
 console.log("Connecting ...");
 let newConnection = connect();
 setupInput(newConnection);
-
-// module.exports = connect;
