7a1be1a896b718b45a3fd89d910d814082167145
SinaSS77 <s.molaverdikhani>
2023-01-18 18:59:22 -0800

    functions for making a connection includs conn

--- client.js
+++ client.js
@@ -15,15 +15,7 @@ const connect = function () {
   conn.on("connect", () => {
     conn.write("Name: SIN");
     conn.write("Say: Beep");
-    // setTimeout(() => conn.write("Move: up"), 5000);
-    // conn.write("Move: down"); // - move down one square (unless facing up)
-    // conn.write("Move: left"); // - move left one square (unless facing right)
-    // conn.write("Move: right"); // - move left one square (unless facing left)
   });
-
-  // conn.on("data", (data) => {
-  //   console.log("Server says: ", data);
-  // });
 
   return conn;
 };
